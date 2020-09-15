f87d3953e29bf2f7e59a8c4fec5cb3471742d71c
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2020-09-14 20:39:57 -0500

    Remove intro stuff and add mdx support

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>This is my website</h1>
+    <p>There is nothing here. Why are you here?</p>
+    <p>Checkout some pages I didn't make:</p>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <p style={{ marginTop: "2rem" }}>I did make this, it's very nice</p>
+    <Link to="/a-post/">Check it out</Link>
   </Layout>
 )
 
