bd83815dacfdf1e2fe7119e1d22ec9fe7c5d5592
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2020-09-14 20:24:32 -0500

    Change the default details

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Brooks Lybrand's Website`,
+    description: `It's like a website, but it's not very good`,
+    author: `@brookslybrand`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
