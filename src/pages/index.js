bcd687fa94aff0d66dfd107426476633711a1f1f
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2020-09-19 12:21:57 -0500

    Add eslint and todos.mdx

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,18 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>This is my website</h1>
+    <marquee>
+      🚧 Warning: This site is under construction in consequentially horrible 🚧
+    </marquee>
+    <Link to="/todos/">
+      <p>I've got some things I want to work on here</p>
+    </Link>
     <p>There is nothing here. Why are you here?</p>
     <p>Checkout some pages I didn't make:</p>
     <Link to="/page-2/">Go to page 2</Link> <br />
